8af1b79026cf6f95a32c7c729d63cfb7d14b0b7d
tranviethoangsmile <81605215+tranviethoangsmile@users.noreply.github.com>
2021-04-12 20:35:58 +0700

    ok

---
+++ luyen_tap_javascript/thuc_hanh_javascript/indexx.js
@@ -0,0 +1,17 @@
+function showAccount(){
+    message = document.getElementById("fulname").value;
+    message = document.getElementById("fuldate").value;
+    message = document.getElementById("acc").value;
+    message = document.getElementById("mk").value;
+    if(message==''){
+    alert('Nhập đầy đủ thông tin');
+    Exit;
+    }
+
+alert('đăng kí thành công');
+
+}
+function showMessage(){
+    alert('nhập lại bạn nhé');
+    exit;
+}
